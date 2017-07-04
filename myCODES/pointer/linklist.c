e726ec93d269c1e54bb985384172ba15836e5c85
shawnluo <andersluoxt@gmail.com>
2017-07-03 17:21:15 -0700

    linklist.c

---
+++ myCODES/pointer/linklist.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct student
+{
+	int id;
+	char *name;
+	struct student *p;
+} STU;
+
+int main(void)
+{
+	STU s1 = 
+		{16, "shawn"};
+
+	printf("name: %s\n", s1.name);
+
+	//STU *s2 =
+	//	{};
+			
+	return 0;
+}
