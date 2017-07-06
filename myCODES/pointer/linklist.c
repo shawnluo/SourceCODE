4d17df32ebfae9561334736b519d2a950c51c33f
shawnluo <andersluoxt@gmail.com>
2017-07-05 21:02:57 -0700

    linklist.c

--- myCODES/pointer/linklist.c
+++ myCODES/pointer/linklist.c
@@ -5,19 +5,113 @@ typedef struct student
 {
 	int id;
 	char *name;
-	struct student *p;
+	struct student *next;
 } STU;
+
+#define LEN sizeof(STU)
+
+STU *creat_Linklist()
+{
+	int i = 0;
+	STU *head;
+	head = (STU*)malloc(LEN);
+	head->id = 3;
+	head->name = "good";
+	head->next = NULL;
+	
+//	printf("name:\t%s\n", head->name);
+//	printf("id:\t%d\n", head->id);
+
+	STU *p1, *p2;
+	p1 = p2 = head;
+
+	while(i++ < 4)
+	{
+		p1 = (STU *)malloc(LEN);
+		p1->id = i * 100;
+		p2->next = p1;
+		p2 = p1;
+	}
+	p1 = NULL;
+	
+	return head;
+}
+
+
+STU *insert_Linklist(STU *head)
+{
+	STU *p1, *p2, *tmp;
+	int NUM = 300;
+
+	p1 = head;
+
+	if(p1->id == NUM)
+	{
+		p1 = (STU *)malloc(LEN);
+		p1->id = 6000;
+		p1->next = head;
+		head = p1;
+	}
+	
+	while(p1->next->id != NUM)
+	{		
+		p1 = p1->next;
+	}
+
+	if(p1 != NULL)
+	{
+//		printf("\t%d\n", p1->id);
+		tmp = p1->next;
+
+		p2 = (STU *)malloc(LEN);
+		p2->id = 6000;
+		p2->next = p1->next;
+		p1->next = p2;
+	}
+	
+	return head;
+}
+
+void free_Linklist(STU *head)
+{
+	STU *p1 = head;
+
+	while(p1->next != NULL)
+	{
+		free(p1);
+		p1 = p1->next;
+	}
+	
+	return head;
+}
+
+void showme(STU *head)
+{
+	STU *p = head;
+	
+	while(p != NULL)
+	{
+		printf("\t%d\n", p->id);
+		p = p->next;
+	}
+}
 
 int main(void)
 {
 	STU s1 = 
 		{16, "shawn"};
 
-	printf("name: %s\n", s1.name);
+//	printf("name: \t%s\n", s1.name);
 
-	creat_Linklist();
-	//STU *s2 =
-	//	{};
-			
+	STU *head, *p;
+
+	head = creat_Linklist();
+//	showme(head);
+
+	head = insert_Linklist(head);
+	showme(head);
+
+//	free_Linklist(head);
+	
 	return 0;
 }
