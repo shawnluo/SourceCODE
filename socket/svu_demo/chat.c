06381fee28af58c5e98e7e67f05459fde2f18ed6
wallace <wallace@localhost.localdomain>
2017-07-30 10:17:17 -0700

    getLocalIpaddress.c pointer

--- socket/svu_demo/chat.c
+++ socket/svu_demo/chat.c
@@ -6,8 +6,6 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
-void *client(void *arg);
-
 
 int clientMode = 0;
 
@@ -22,6 +20,119 @@ typedef struct
 	char *ipaddress;
 	int port;
 } *PARA;
+
+
+
+#define xstr(s)    str(s)
+#define str(s)     #s
+
+#define ARP_CACHE          "/proc/net/arp"
+#define ARP_STRING_LEN     1023
+#define ARP_BUFFER_LEN     (ARP_STRING_LEN + 1)
+
+/* Format for fscanf() to read the 1st, 4th, and 6th space-delimited fields */
+#define ARP_LINE_FORMAT    "%" xstr(ARP_STRING_LEN) "s %*s %*s " \
+    "%" xstr(ARP_STRING_LEN) "s %*s "                            \
+    "%" xstr(ARP_STRING_LEN) "s"
+
+
+/********************************************
+ *
+ ********************************************/
+char *getLocalDevice()
+{
+    FILE *arpCache = fopen(ARP_CACHE, "r");
+
+    if (!arpCache)
+    {
+        perror("Arp Cache: Failed to open file \"" ARP_CACHE "\"");
+        return 1;
+    }
+
+    /* Ignore the first line, which contains the header */
+    char header[ARP_BUFFER_LEN];
+    if (!fgets(header, sizeof(header), arpCache))
+    {
+        return 1;
+    }
+
+    char ipAddr[ARP_BUFFER_LEN], hwAddr[ARP_BUFFER_LEN], device[ARP_BUFFER_LEN];
+    int  count = 0;
+    while (3 == fscanf(arpCache, ARP_LINE_FORMAT, ipAddr, hwAddr, device))	//return 3 since there are 3 varialble 
+    {
+        printf("%03d: Mac Address of [%s] on [%s] is \"%s\"\n",
+               ++count, ipAddr, device, hwAddr);
+    }
+    fclose(arpCache);
+    //return 0;
+
+	return ipAddr;
+}
+
+
+
+
+void *client(void *arg)
+{
+    int                sockfd, portno, n, option = 1;
+
+    struct sockaddr_in serv_addr;
+    struct hostent     *server;
+
+    char               buffer[256];
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+    portno = (int)arg;//atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error("ERROR opening socket");
+    }
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(1));
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        error("ERROR connecting");
+    }
+    printf("Please enter the message: ");
+    while (1)
+    {
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+        {
+            error("ERROR writing to socket");
+        }
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
+        if (n < 0)
+        {
+            error("ERROR reading from socket");
+        }
+        printf("%s\n", buffer);
+    }
+	
+    pthread_exit((void *)0);
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -106,67 +217,5 @@ int main(int argc, char *argv[])
     pthread_join(tidClie, NULL);
 	
     return 0;
-}
-
-
-
-void *client(void *arg)
-{
-    int                sockfd, portno, n, option = 1;
-
-    struct sockaddr_in serv_addr;
-    struct hostent     *server;
-
-    char               buffer[256];
-
-    if (argc < 3)
-    {
-        fprintf(stderr, "usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
-    portno = (int)arg;//atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        error("ERROR opening socket");
-    }
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(1));
-
-    server = gethostbyname(argv[1]);
-    if (server == NULL)
-    {
-        fprintf(stderr, "ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        error("ERROR connecting");
-    }
-    printf("Please enter the message: ");
-    while (1)
-    {
-        bzero(buffer, 256);
-        fgets(buffer, 255, stdin);
-        n = write(sockfd, buffer, strlen(buffer));
-        if (n < 0)
-        {
-            error("ERROR writing to socket");
-        }
-        bzero(buffer, 256);
-        n = read(sockfd, buffer, 255);
-        if (n < 0)
-        {
-            error("ERROR reading from socket");
-        }
-        printf("%s\n", buffer);
-    }
-	
-    pthread_exit((void *)0);
 }
 
