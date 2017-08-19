edbbac3a5ca3afadd19059d1876a5404bd865b60
wallace <wallace@localhost.localdomain>
2017-08-18 20:09:18 -0700

    test.c

--- tmp/test.c
+++ tmp/test.c
@@ -10,125 +10,53 @@
 #include "math.h"
 
 #include <stdbool.h>
-#include "color.h"
+//#include "color.h"
 
-
+/*
 void print_trace()
 {
-    my_trace(BRIGHT, GREEN, BLACK, "good\n");
-}
+//    my_trace(BRIGHT, GREEN, BLACK, "good\n");
+}*/
 
-#if 1
-/***************************************************************************************
- *	write a general purpose function, calculate (0, 1)sinxdx, (-1, 1)cosxdx, (0, 2)exdx.
- ****************************************************************************************/
- 
-float integral(float (*p)(float), float a, float b, int n)
-{
-    int   i;
-    float x, h, s;
-
-    h = (b - a) / n;
-    x = a;
-    s = 0;
-    for (i = 0; i < n; i++)
-    {
-        x = x + h;
-        s = s + (*p)(x) * h;
-    }
-    return(s);
-}
-
-float fsin(float x)
-{
-    return sin(x);
-}
-float fcos(float x)
-{
-    return cos(x);
-}
-float fexp(float x)
-{
-    return exp(x);
-}
-
-
-//typedef float (*fun)(float);
-
+#if 0
 
 int main(void)
 {
-    float a1, b1, a2, b2, a3, b3, c, (*p)(float);
-    int   n = 20;
+/*
+	char *p1 = "gooday";
+	char *p2 = "yes";
+
+	strcpy(p1, p2);
+
+	printf("%s\n", p1);
+*/
+	char text[] = "This is a list of lists";
+	char *find = "list";
+	char *pos = NULL;
+	pos = strstr(text, find);
+	printf("pos = %s\n", pos);
+
+	strncpy(pos, "text", 4);
+	printf("pos = %s\n", pos);
 	
-    printf("enter a1,b1: ");                   
-    scanf("%f %f", &a1, &b1);
-	
-    printf("enter a2,b2: ");
-    scanf("%f %f", &a2, &b2);
-	
-    printf("enter a3,b3: ");
-    scanf("%f %f", &a3, &b3);
-
-    p = fsin;
-    c = integral(p, a1, b1, n);
-    printf("sin(x)=%f\n", c);
-
-    p = fcos;
-    c = integral(p, a2, b2, n);
-    printf("cos(x)=%f\n", c);
-
-    p = fexp;
-    c = integral(p, a3, b3, n);
-    printf("exp(x)=%f\n", c);
-
-    return 0;
+	return 0;
 }
+
 #else
 
-typedef float (*fun)(float);
-
-float intergral(fun Fun, float a, float b)
-{
-	
-	return 	Fun(a) + b;
-}
-
-int myFun(float i)
-{
-	printf("%f\n", i);
-	return 0;
-}
-
-float fsin(float x)
-{
-	return x;//sin(x);
-}
-
-float fcos(float x)
-{
-	return x * x;
-}
-
 int main(void)
 {
-	float ret, a, b;
-	fun Fun;	// = myFun;
+	char *p1 = (char *)malloc(100);
+	strcpy(p1, "showmethe");
+	printf("%s\n", p1);
 
-	scanf("%f %f", &a, &b);
-	Fun = fsin;
-	ret = intergral(Fun, a, b);
-	printf("%f\n", ret);
+/*
+	char *p2 = "good";
 
-	Fun = fcos;
-	ret = intergral(Fun, a, b);
-	printf("%f\n", ret);
+	strcpy(p1, p2);
 
-	Fun(10.9809);
-
-//	ret = integral(fun);
-
+	printf("%s\n", p1);*/
 	return 0;
 }
-#endif
 
+#endif
