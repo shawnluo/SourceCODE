486bfc2b149be525eb2974b0fd2d3cb11e768aff
shawnluo <andersluoxt@gmail.com>
2019-10-06 08:57:29 -0700

    add python sorting and tricky folder

---
+++ python/sorting/bubble_sort.py
@@ -0,0 +1,11 @@
+
+# -*- coding:ut-f -*-
+
+import math
+
+if __name__ == '__main__':
+	testlist = [2, 5, 7, 1, 9, 4, 6, 0, 12, 35, 7]
+	bubble = BubbleSortMethod(testlist)
+	sortlist = bubble.bubble_sort1()
+	
+
