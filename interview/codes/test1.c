8e65f584a2c78354db16f24b3ff4d52292a1aeb7
Goodaytodie <andersluo@hotmail.com>
2022-02-18 22:19:08 -0800

    update test1.c

--- interview/codes/test1.c
+++ interview/codes/test1.c
@@ -3,59 +3,46 @@
 #include <assert.h>
 #include <string.h>
 
-int cmp_string(const char *a, const char *b)
+/*
+    find all the substrings, save them to the substring[] dynamatically;
+
+    1234
+
+    - 1, 12, 123, 1234
+    - 2, 23, 234
+    - 3, 34
+    - 4
+*/
+
+
+
+void find_all_substr(const char *str)
 {
-    assert(a);
-    assert(b);
-    assert(1 > 0);
+    char **substr;
+    int len = strlen(str);
+    int i, j, k = 0;
 
-    int size_a = strlen(a);
-    int size_b = strlen(b);
-
-    while((*a++ == *b++) && size_a != 0 && size_b != 0)
+    for(i = 0; i < len; i++)
     {
-        size_a--;
-        size_b--;
+        for(j = i; j < len; j++)
+        {
+            for(k = 0; k < j; k++)
+            {
+                printf("%c", str[k]);
+            }
+            printf("\t");
+        }
+        printf("\n");
     }
-
-    if((size_a == 0) && (size_b == 0))
-    {
-        printf(" - 111\n");
-        return 0;
-    }
-    else if(size_a == 0)
-    {
-        printf(" - 222\n");
-        return -1;
-    }
-    else if(size_b == 0)
-    {
-        printf(" - 333\n");
-        return 1;
-    }
-    else
-    {
-        printf(" - 444\n");
-        return *a > *b ? 1 : -1;
-    }
-
-    return 0;
-}
-
-void show(char *str, char *str2)
-{
-    //str = str2;
-    strcpy(str, str2);
-    printf("%s\n", str);
 }
 
 int main(void)
 {
     char str[10] = "show";
-    char str2[10] = "me";
-    show(str, str2);
 
-    printf("2 - %s\n", str);
+    find_all_substr(str);
+
+    //printf("2 - %s\n", str);
     //for(int i = 0; i < sizeof(str2); i++)
     //printf("%c\t", str2[i]);
     //printf("\n");
