606aa70604e0e1a987f2315d74668160dccb3e37
Goodaytodie <andersluo@hotmail.com>
2022-02-26 01:18:20 -0800

    practise

--- interview/codes/test.c
+++ interview/codes/test.c
@@ -31,18 +31,18 @@ int free_ll(pData p)
 
 void print_ll(pData p)
 {
-    printf("print ll!\n");
+    printf("print ll: ");
     while(p != NULL)
     {
-        printf("p->i = %d\n", p->i);
+        printf("%d ", p->i);
         p = p->next;
     }
+    printf("\n");
 }
 
-pData create_ll()
-//void create_ll(pData *ppHead)
+
+int create_ll(pData *ppHead)
 {
-    pData pHead = NULL;//*ppHead;
     int arr[] = {1, 2, 3, 4, 5};
     int len = sizeof(arr) / sizeof(arr[0]);
     pData p = NULL;
@@ -50,22 +50,121 @@ pData create_ll()
     while(len > 0)
     {
         p = (pData)malloc(LEN);
+        if(!p)
+        {
+            printf("malloc failed!\n");
+            return -1;
+        }
         memset(p, 0, LEN);
         p->i = arr[len - 1];
-        p->next = pHead;
-        pHead = p;
+        p->next = *ppHead;
+        *ppHead = p;
         len--;
     }
-    //print_ll(pHead);
-    return pHead;
+
+    return 0;
+}
+
+int del_node(pData *ppHead, int target)
+{
+    if(*ppHead == NULL)
+    {
+        printf("p is empty!\n");
+        return -1;
+    }
+    pData *pp = ppHead;
+    
+    while(*pp != NULL && (*pp)->i != target)
+    {
+        pp = &((*pp)->next);
+    }
+
+    if(*pp == NULL)
+    {
+        printf("can NOT find the node!\n");
+        return 1;
+    }
+
+    pData pTmp = *pp;
+    *pp = (*pp)->next;
+    free(pTmp);
+
+    return 0;
+}
+
+int ins_node_front(pData *ppHead, int newData, int target)
+{
+    if(*ppHead == NULL)
+    {
+        printf("ll is empty!\n");
+        return -1;
+    }
+
+    pData *pp = ppHead;
+    while((*pp) != NULL && (*pp)->i != target)
+    {
+        pp = &((*pp)->next);
+    }
+
+    if(*pp == NULL)
+    {
+        printf("can NOT find the target!\n");
+        return 1;
+    }
+
+    pData newNode = (pData)malloc(LEN);
+    memset(newNode, 0, LEN);
+
+    newNode->i = newData;
+    newNode->next = *pp;
+    *pp = newNode;
+
+    return 0;    
+}
+
+int ins_node_behind(pData *ppHead, int newData, int target)
+{
+    if(*ppHead == NULL)
+    {
+        printf("ll is empty!\n");
+        return -1;
+    }
+
+    pData p = *ppHead;
+    while(p != NULL && p->i != target)
+    {
+        p = p->next;
+    }
+    pData newNode = (pData)malloc(LEN);
+    newNode->i = newData;
+    newNode->next = p->next;
+    p->next = newNode;
+
+    return 0;
+}
+
+int reverse_ll(pData pHead)
+{
+    assert(pHead);
+
+    pData p = pHead;
 }
 
 int main()
 {
     pData pHead = NULL;
-    pHead = create_ll();
-    //create_ll(&pHead);
+
+    int ret = create_ll(&pHead);
     //printf("ret = %d\n", ret);
+    print_ll(pHead);
+
+    //del_node(&pHead, 1);
+    //print_ll(pHead);
+
+    ins_node_front(&pHead, 100, 1);
+    print_ll(pHead);
+
+    ins_node_behind(&pHead, 1000, 1);
     print_ll(pHead);
 
     free_ll(pHead);
