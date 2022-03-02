9d54999aed3cce6e5b248070473dba2f00db38ce
goodaytodie <andersluo@hotmail.com>
2022-03-02 14:12:57 -0800

    test

--- interview/codes/test1.c
+++ interview/codes/test1.c
@@ -4,76 +4,10 @@
 #include <assert.h>
 
 
-#define SIZE 10
-
-#if 1
-int main(void)
-{
-    int **pArr = NULL;
-
-    pArr = (int **)malloc(sizeof(int *) * SIZE);
-    memset(pArr, 0, sizeof(int *) * SIZE);
-    
-    int index = 0;
-    pArr[index] = (int *)malloc(sizeof(int) * SIZE);
-    memset(pArr[index], 0, sizeof(int) * SIZE);
-
-    for(int i = 0; i < SIZE; i++)
-    {
-        pArr[index][i] = i;
-    }
-
-    for(int i = 0; i < SIZE; i++)
-    {
-        printf("%d ", pArr[index][i]);
-    }
-    printf("\n");
-
-    free(pArr[index]);
-    free(pArr);
-
-    return 0;
-}
-
-#else
 
 int main(void)
 {
-    int (*pArr)[SIZE] = NULL;
-
-    pArr = (int *)malloc(sizeof(int) * SIZE);
-    
-    for(int i = 0; i < SIZE; i++)
-    {
-        (*pArr)[i] = i;
-    }
-
-    for(int i = 0; i < SIZE; i++)
-    {
-        printf("%d ", (*pArr)[i]);
-    }
-    printf("\n");
-
-    int (*pTmp)[SIZE] = pArr;
-
-    pArr++;
-    pArr = (int *)malloc(sizeof(int) * SIZE);
-    
-    for(int i = 0; i < SIZE; i++)
-    {
-        (*pArr)[i] = i * 10;
-    }
-
-    for(int i = 0; i < SIZE; i++)
-    {
-        printf("%d ", (*pArr)[i]);
-    }
-    printf("\n");
-
-    free(pTmp);
-    free(pArr);
-
+   int i = ~(0/2);
+    printf("%ld\n", i);
     return 0;
 }
-
-#endif
