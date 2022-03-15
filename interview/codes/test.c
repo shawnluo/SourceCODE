ceb157d02fd00207162c83ba0989187478649bb7
Goodaytodie <andersluo@hotmail.com>
2022-03-14 18:58:54 -0700

    tst

--- interview/codes/test.c
+++ interview/codes/test.c
@@ -1,99 +1,137 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <wait.h>
-#include <signal.h>
-#include <errno.h>
-#include <sys/select.h>
-#include <sys/time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include <pthread.h>
+#include <time.h>
 #include <unistd.h>
- 
-#define MAXBUF 256
- 
-void child_process(void)
+#include <semaphore.h>
+#include <sys/ipc.h>
+#include <fcntl.h>
+
+/*
+    producer and consumer:
+    1. buffer[10]
+    2. producer pushes to buffer
+    3. consumer pops from buffer
+    4. producer pauses pushing when buffer is full
+    5. consumer pauses popping when buffer is empty
+
+    solution:
+        1. mutex for 2 threads to access the same buffer
+        2. synchnization of produce and consume
+            full    0
+            avail   N - the empty unit number
+*/
+
+#define N 5
+#define FIFO "myfifo"
+
+time_t end_time;
+int fd;
+char buf_r[100];
+sem_t mutex, avail, full;
+
+void productor(void *arg);
+void consumer(void *arg);
+
+int main(void)
 {
-    sleep(2);
-    char msg[MAXBUF];
-    struct sockaddr_in addr = {0};
-    int n, sockfd,num=1;
+    pthread_t id1, id2;
+    end_time = time(NULL) + 30;
+    int ret;
 
-    srandom(getpid());
-    /* Create socket and connect to server */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(2000);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
-
-    printf("child {%d} connected \n", getpid());
-    while(1)
+    if((mkfifo(FIFO, 0666) < 0) && (errno != EEXIST))
     {
-        int sl = (random() % 10 ) + 1;
-        num++;
-        sleep(sl);
-        sprintf (msg, "Test message %d from client %d", num, getpid());
-        n = write(sockfd, msg, strlen(msg));	/* Send message */
+        printf("cannot create fifoserver\n");
+    }
+    printf("Preparing for reading bytes...\n");
+    memset(buf_r, 0, sizeof(buf_r));
+
+    fd = open(FIFO, O_RDWR|O_NONBLOCK, 0);
+    if(fd == -1)
+    {
+        perror("open");
+        exit(1);
+    }
+
+    ret = sem_init(&mutex, 0, 1);
+    ret = sem_init(&avail, 0, N);   //N empty space
+    ret = sem_init(&full, 0, 0);
+    if(ret != 0)
+    {
+        perror("sem_init");
+    }
+
+    ret = pthread_create(&id1, NULL, (void *)productor, NULL);
+    if(ret != 0)
+    {
+        perror("pthread cread1");
+    }
+    ret = pthread_create(&id2, NULL, (void *)consumer, NULL);
+    if(ret != 0)
+    {
+        perror("pthread cread2");
+    }
+
+    pthread_join(id1, NULL);
+    pthread_join(id2, NULL);
+
+    return (0);
+}
+
+void productor(void *arg)
+{
+    int i, nwrite, val;
+    while(time(NULL) < end_time)
+    {
+        sem_wait(&avail);   //N empty space
+        sem_getvalue(&avail, &val);
+        printf("p - %d\n", val);
+
+        sem_wait(&mutex);   //it does NOT matter which thread go first
+
+        if((nwrite = write(fd, "hello", 5)) == -1)
+        {
+            printf("write fail, data unavailable!\n");
+        }
+        else
+        {
+            sleep(1);
+            printf("write hello to the FIFO. buf remain: %d\n", strlen(buf_r));
+        }
+        sem_post(&full);
+        sem_post(&mutex);
+        sleep(1);
     }
 }
- 
-int main()
+
+void consumer(void *arg)
 {
-    char buffer[MAXBUF];
-    int fds[5];
-    struct sockaddr_in addr;
-    struct sockaddr_in client;
-    int addrlen, n,i,max=0;;
-    int sockfd, commfd;
-    fd_set rset;
-
-    for(i=0;i<5;i++)
+    int nolock = 0;
+    int ret, nread, val;
+    int n = 1;
+    while(time(NULL) < end_time)
     {
-        if(fork() == 0)
+        sem_wait(&full);
+        sem_wait(&mutex);
+
+        sem_getvalue(&avail, &val);
+        printf("c - %d\n", val);
+
+        memset(buf_r, 0, sizeof(buf_r));
+        if((nread = read(fd, buf_r, n)) == -1)
         {
-            child_process();
-            exit(0);
+            printf("no data yet\n");
         }
-    }
+        sleep(3);
+        printf("read %s from FIFO, remain: %d\n", buf_r, strlen(buf_r));
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(2000);
-    addr.sin_addr.s_addr = INADDR_ANY;
-    bind(sockfd,(struct sockaddr*)&addr ,sizeof(addr));
-    listen (sockfd, 5); 
- 
-    for (i=0;i<5;i++) 
-    {
-        memset(&client, 0, sizeof (client));
-        addrlen = sizeof(client);
-        fds[i] = accept(sockfd,(struct sockaddr*)&client, &addrlen);
-        if(fds[i] > max)
-        max = fds[i];
+        sem_post(&avail);
+        sem_post(&mutex);
+        sleep(1);
     }
-
-    while(1)
-    {
-	    FD_ZERO(&rset);
-  	    for (i = 0; i< 5; i++ ) 
-        {
-  		    FD_SET(fds[i],&rset);
-  	    }
- 
-        puts("round again");
-        select(max+1, &rset, NULL, NULL, NULL);
- 
-        for(i=0;i<5;i++)
-        {
-            if (FD_ISSET(fds[i], &rset))
-            {
-                memset(buffer,0,MAXBUF);
-                read(fds[i], buffer, MAXBUF);
-                puts(buffer);
-            }
-        }
-    }
-    return 0;
 }
--- interview/codes/test1.c
+++ interview/codes/test1.c
@@ -1,349 +1,79 @@
-#include <stdio.h> 
-#include <pthread.h> 
+/*信号量实现 多生产者 多消费者问题*/
+
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include <unistd.h>
-#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
 
-typedef struct data{
-    int num;                //4 bytes
-//    int x;                  //4 bytes
-    struct data *next;      //8 bytes
+#define NUM 5
 
-} data, *pData;
+int queue[NUM];                     //全局数组实现环形队列
+sem_t blank_number, product_number; //空格数信号量, 产品数信号量
 
-#define SIZE sizeof(data)
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 
-void showme(pData head)
+int prodeucer_i = 0;
+int consumer_i = 0;
+void *producer(void *arg)
 {
-    if(!head)
+    while (1)
     {
-        printf("empty!\n");
-        return;
+        sem_wait(&blank_number); //生产者将空格子数--,为0则阻塞等待
+        pthread_mutex_lock(&mutex1);
+        int data = rand() % 1000 + 1;
+        queue[prodeucer_i] = data;             //生产一个产品
+        prodeucer_i = (prodeucer_i + 1) % NUM; //借助下标实现环形
+        pthread_mutex_unlock(&mutex1);
+        sem_post(&product_number); //将产品数++
+
+        printf("----Produce%d---%d\n", (int)arg, data);
+
+        sleep(rand() % 3);
     }
-    assert(head);
-    while(head)
-    {
-        printf("%d ", head->num);
-        head = head->next;
-    }
-    printf("\n");
 }
 
-pData create_linkedlist(int *arr, int size)
+void *consumer(void *arg)
 {
-    pData head = NULL, p = NULL;
-    while(size--)
+    while (1)
     {
-        p = (pData)malloc(SIZE);
-        p->num = arr[size];
-        p->next = head;
-        head = p;
-    }
+        sem_wait(&product_number); //消费者将产品数--,为0则阻塞等待
+        pthread_mutex_lock(&mutex2);
+        int data = queue[consumer_i];
+        queue[consumer_i] = 0; //消费一个产品
+        consumer_i = (consumer_i + 1) % NUM;
+        pthread_mutex_unlock(&mutex2);
+        sem_post(&blank_number); //消费掉以后,将空格子数++
 
-    return head;
+        printf("-Consume%d---%d\n", (int)arg, data);
+
+        sleep(rand() % 3);
+    }
 }
 
-int insert_ll_infront(pData *ppHead, int target, int newData)
+int main(int argc, char *argv[])
 {
-    assert(*ppHead);
+    pthread_t pid1, pid2, cid1, cid2;
 
-    pData *pp = ppHead;
-    while(*pp && (*pp)->num != target)
-    {
-        pp = &(*pp)->next;
-    }
-    if(*pp == NULL)
-    {
-        printf("can not find it\n");
-        return -1;
-    }
-    pData newNode = (pData)malloc(SIZE);
-    newNode->num = newData;
-    newNode->next = *pp;
-    *pp = newNode;
+    sem_init(&blank_number, 0, NUM); //初始化空格子信号量为5
+    sem_init(&product_number, 0, 0); //产品数为0
 
-    return 0;
-}
+    pthread_create(&pid1, NULL, producer, (void *)1);
+    pthread_create(&pid2, NULL, producer, (void *)2);
+    pthread_create(&cid1, NULL, consumer, (void *)1);
+    pthread_create(&cid2, NULL, consumer, (void *)2);
 
-int insert_ll_after(pData pHead, int target, int newData)
-{
-    if(!pHead)
-    {
-        printf("Empty!\n");
-        return -1;
-    }
-    assert(pHead);
-    pData p = pHead;
-    while(p && p->num != target)
-    {
-        p = p->next;
-    }
+    pthread_join(pid1, NULL);
+    pthread_join(pid2, NULL);
+    pthread_join(cid1, NULL);
+    pthread_join(cid2, NULL);
 
-    if(!p)
-    {
-        printf("can not find it\n");
-        return -1;
-    }
-    pData tmp = (pData)malloc(SIZE);
-    tmp->num = newData;
-    tmp->next = p->next;
-    p->next = tmp;
+    sem_destroy(&blank_number);
+    sem_destroy(&product_number);
 
-    return 0;
-}
-
-int del_ll_node(pData *ppHead, int target)
-{
-    assert(*ppHead);
-    pData *pp = ppHead;
-    while(*pp && (*pp)->num != target)
-    {
-        pp = &(*pp)->next;
-    }
-
-    if(!*pp)
-    {
-        printf("can not find it\n");
-        return -1;
-    }
-    pData tmp = *pp;
-    *pp = (*pp)->next;
-    free(tmp);
-
-    return 0;
-}
-
-int del_ll_all(pData pHead)
-{
-    assert(pHead);
-    pData p = pHead;
-
-    while(pHead)
-    {
-        p = pHead;
-        pHead = pHead->next;
-        free(p);
-    }
-
-    //*ppHead = NULL;
-    return 0;
-}
-
-int add_newNode_ll(pData *ppHead, int newData)
-{
-    pData p = NULL;
-    if(!*ppHead)
-    {
-        *ppHead = (pData)malloc(SIZE);
-        (*ppHead)->num = newData;
-        (*ppHead)->next = NULL;
-        return 0;
-    }
-
-    p = *ppHead;
-    while(p->next != NULL)
-    {
-        p = p->next;
-    }
-    pData newNode = NULL;
-    newNode = (pData)malloc(SIZE);
-    newNode->num = newData;
-    newNode->next = NULL;
-    p->next = newNode;
-
-    return 0;
-}
-
-/*
-    linked list addation
-*/
-pData add_2_ll(pData pHead1, pData pHead2)
-{
-    int inc = 0, newData = 0;
-    pData p1 = pHead1, p2 = pHead2, pHead3 = NULL;
-    for(p1 = pHead1, p2 = pHead2; p1 != NULL || p2 != NULL;)
-    {
-        if(!p1)
-        {
-            //p1->num = 0;
-            newData = (p2->num) + inc;
-        }
-        else if(!p2)
-        {
-            newData = (p1->num) + inc;
-            //p2->num = 0;
-        }
-        else
-        {
-            newData = (p1->num) + (p2->num) + inc;
-        }
-        //printf("%d, ", newData);
-        if(newData >= 10)
-        {
-            newData = newData - 10;
-            inc = 1;
-        }
-        else
-        {
-            newData = newData;
-            inc = 0;
-        }
-
-        add_newNode_ll(&pHead3, newData);
-
-        if(!p1)
-        {
-            p2 = p2->next;
-        }
-        else if(!p2)
-        {
-            p1 = p1->next;
-        }
-        else
-        {
-            p1 = p1->next;
-            p2 = p2->next;
-        }
-    }
-    if(inc == 1)
-    {
-        add_newNode_ll(&pHead3, inc);
-    }
-    showme(pHead3);
-    return pHead3;
-}
-
-int merge_ll(pData head1, pData head2)
-{
-    pData p1 = NULL;
-    pData p2 = NULL;
-    pData p3 = NULL;
-
-    for(p1 = head1, p2 = head2; p1 != NULL || p2 != NULL;)
-    {
-        if(p1 == NULL)
-        {
-            add_newNode_ll(&p3, p2->num);
-            p2 = p2->next;
-            continue;
-        }
-        else if(p2 == NULL)
-        {
-            add_newNode_ll(&p3, p1->num);
-            p1 = p1->next;
-            continue;
-        }
-
-        if(p1->num <= p2->num)
-        {
-            add_newNode_ll(&p3, p1->num);
-            p1 = p1->next;
-        }
-        else
-        {
-            add_newNode_ll(&p3, p2->num);
-            p2 = p2->next;
-        }
-    }
-    showme(p3);
-
-    return 0;
-}
-
-
-int create_circular_ll(pData *ppHead)
-{
-    pData p = *ppHead;
-    while(p->next)
-    {
-        p = p->next;
-    }
-
-    p->next = (*ppHead)->next->next;
-
-    return 0;
-}
-
-
-int find_circular_entry_ll(pData pHead)
-{
-    pData p1 = pHead;
-    pData p2 = pHead;
-    //if(p1->next == p1)
-    //{
-    //    return p1->num;
-    //}
-
-    while(p2->next != NULL)
-    {
-        p1 = p1->next;
-        p2 = p2->next->next;
-        if(p1 == p2)
-        {
-            break;
-        }
-    }
-    if(p2->next == NULL)
-    {
-        printf("no circular!\n");
-        return -1;
-    }
-
-    p1 = pHead;
-    while(p1 != p2)
-    {
-        p1 = p1->next;
-        p2 = p2->next;
-    }
-    return p1->num;
-}
-
-
-int main(void)
-{
-    int arr1[] = {2, 4, 14, 71, 125};
-    int arr2[] = {1, 2, 3, 4, 5, 9};
-    int len1 = sizeof(arr1) / sizeof(arr1[0]);
-    int len2 = sizeof(arr2) / sizeof(arr2[0]);
-
-    pData pHead1 = create_linkedlist(arr1, len1);
-    showme(pHead1);
-    //pData pHead2 = create_linkedlist(arr2, len2);
-    //showme(pHead2);
-
-    int ret;
-
-    //ret = insert_ll_infront(&head, 130, 1000);
-    //showme(head);
-
-    //ret = insert_ll_after(head, 500, 2000);
-    //showme(head);
-
-    //ret = del_ll_node(&head, 500);
-    //showme(head);
-
-    //ret = del_ll_all(head);
-    //showme(head);
-
-    //pData head3 = NULL; 
-    //add_newNode_ll(&head3, 1234);
-    //showme(head3);
-
-    //add_newNode_ll(&head3, 5678);
-    //showme(head3);
-
-    //pData pHead3 = add_ll(head1, head2);
-    //showme(pHead3);
-
-    //merge_ll(head1, head2);
-
-    create_circular_ll(&pHead1);
-    //showme(pHead1);
-
-    ret = find_circular_entry_ll(pHead1);
-    printf("%d\n", ret);
+    pthread_mutex_destroy(&mutex1);
+    pthread_mutex_destroy(&mutex2);
 
     return 0;
 }
