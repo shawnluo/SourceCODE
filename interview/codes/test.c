d8b202e48113cc6ff2670b35e0291f6350294a03
Goodaytodie <andersluo@hotmail.com>
2022-03-18 23:19:22 -0700

    test

--- interview/codes/test.c
+++ interview/codes/test.c
@@ -14,88 +14,14 @@
 #include <assert.h>
 
 
-/*
-    global:
-        buff[]
-        size = 100
-        stack[3]: [0 ~ 100),  [100, 200), [200, 300)
-        pointer to the top of the stack
+
+/*  5_3
+    
+
 */
-
-#define SIZE 100
-int buff[SIZE] = {0};
-int stack_top = 0;    //point to the top of stack
-
-void push(int data)
-{
-    int index = stack_top + 1;
-    if(index >= SIZE)
-    {
-        return;
-    }
-    buff[index] = data;
-    stack_top++;
-}
-
-int pop()
-{
-    int value;
-    int index = stack_top;
-    value = buff[index];
-
-    index = stack_top - 1;
-    if(index < 0)
-    {
-        return -1;
-    }
-    stack_top--;
-
-    return value;
-}
-
-int peek()
-{
-    int index = stack_top;
-    printf("%d\n", buff[index]);
-    return buff[index];
-}
-
-int isEmpty()
-{
-
-}
-
-int isFull()
-{
-
-}
-
-
-int create_stacks()
-{
-
-}
-
 
 int main(void)
 {
-    push(99);
-    push(21);
-    push(87);
-    int val = peek();
-    //printf("%d\n", val);
 
-    pop();
-    peek();
-
-    pop();
-    peek();
-
-    pop();
-    peek();
-
-    pop();
-    peek();
-    
     return 0;
 }
