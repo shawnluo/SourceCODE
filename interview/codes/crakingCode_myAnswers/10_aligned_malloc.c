36378f61e7bf5b4e2af202d2a7644c0cc9425a99
Goodaytodie <andersluo@hotmail.com>
2022-03-27 21:29:40 -0700

    test

--- interview/codes/crakingCode_myAnswers/10_aligned_malloc.c
+++ interview/codes/crakingCode_myAnswers/10_aligned_malloc.c
@@ -13,27 +13,29 @@
 #include <fcntl.h>
 #include <assert.h>
 
+typedef uint16_t offset_t;
 
 void *aligned_malloc(size_t required_bytes, size_t alignment)
 {
     int offset = alignment - 1;
     void *p_addr = NULL;
-
-    if((p_addr = (void *)malloc(required_bytes + offset + sizeof(uint16_t))) == NULL)
+    //need about 2 bytes for storing offset, to get to orig malloc address during aligned_free()
+    if((p_addr = (void *)malloc(required_bytes + offset + sizeof(offset_t))) == NULL)
     {
         return NULL;
     }
 
-    void *aligned_addr = (p_addr + offset) & ~(offset);
-    *((int *)aligned_addr - 1) = aligned_addr - p_addr;
+    void *aligned_addr = (void *)(((size_t)p_addr + offset) & ~(offset));
+    *((size_t *)aligned_addr - 1) = (size_t)(aligned_addr - p_addr);
 
     return aligned_addr;
 }
 
+
 void aligned_free(void *ptr)
 {
     assert(ptr);
-    int offset = *((int *)ptr - 1);
+    offset_t offset = *((offset_t *)ptr - 1);
     void *p = (void *)((int *)ptr - offset);
     free(p);
 }
