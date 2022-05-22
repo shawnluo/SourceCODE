39ea3dd95f377c5c57df01586a3f5ba4e7be5048
goodaytodie <andersluo@hotmail.com>
2022-05-22 12:21:15 -0700

    test

--- interview/codes/test.c
+++ interview/codes/test.c
@@ -15,41 +15,29 @@
 #include <fcntl.h>
 #include <assert.h>
 
-/*
-Design  an  algorithm  and  write  code  to  remove  the  duplicate  
-characters  in  a  string without using any additional buffer  
-NOTE: One or two additional variables are fineAn extra copy of the array is not
-*/
-
-//way1. maintain a string without duplicate
-void remove_dup(char *str) {
-    int i, j;
+int replace_space_with(char *str) {
+    int i, j, k;
     int len = strlen(str);
-    int k = 0;  //without dup
+    int n_space = 0;
 
-    //k = 1;
-    for(i = 1; i < len; i++) {
-        for(j = 0; j <= k; j++) {
-            if(str[i] == str[j]) {
-                break;
-            }
-        }
-        if(j > k) {
-            str[++k] = str[i];
+    for(i = 0; i < len; i++) {
+        if(str[i] == ' ') {
+            n_space++;
         }
     }
-    str[++k] = '\0';
+
+    for(i = len; i >= 0;) {
+        
+    }
+
 }
 
-/*way2. go through the string from left to right, 
-if space then move all the right chars to left
-*/
 
+int main(void) {
+    char str[20] = "123 4  1516 ";
 
-int main() {
-    char str[20] = "1234156";
-
-    remove_dup(str);
+    replace_space_with(str);
+    //printf("ret = %d\n", ret);
     printf("str = %s\n", str);
 
 	return 0;
