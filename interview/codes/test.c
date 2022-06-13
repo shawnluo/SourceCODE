96a77a08d1eebc81bd04f3148440dc05507e8774
goodaytodie <andersluo@hotmail.com>
2022-06-12 22:47:52 -0700

    test 2

--- interview/codes/test.c
+++ interview/codes/test.c
@@ -17,24 +17,11 @@
 
 
 
-<<<<<<< HEAD
-
-
-int main(void) {
-    char *s1[] = {"abc", "good"};
-    //char (*s2)[] = {"123", "4567"};
-
-    char *p = s1[0];
-
-    printf("%s\n", p);
-    printf("%s\n", p + 1);
-=======
 int main(void) {
     char *s1[] = {"abc", "1234", "defghi"};
     char *p = s1[0];
 
     printf("%s\n", s1);
->>>>>>> 6e2c3b4... marswalker test
 
 	return 0;
 }
