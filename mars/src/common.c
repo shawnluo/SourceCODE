72dc2917920d4f2f6790c316a783668d013b4f0e
goodaytodie <andersluo@hotmail.com>
2022-09-11 21:20:50 -0700

    add mars project

---
+++ mars/src/common.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/common.h"
+
+void showme() {
+    printf("show must go on!\n");
+}
---
+++ mars/src/main.c
@@ -0,0 +1,8 @@
+
+#include "../include/common.h"
+
+int main(void) {
+    printf("It's a good day to fight!\n");
+
+    return 0;
+}
