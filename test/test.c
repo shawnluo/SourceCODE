3a61f1b302f13616685db64070d0a5af25a75a27
Goodaytodie <andersluo@hotmail.com>
2022-11-13 19:03:18 -0800

    add

--- test/test.c
+++ test/test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 /*
     step 1: if it hasn't repeat character
@@ -26,19 +27,69 @@ int Has_unqi_char(char *str) {
     return 1;
 }
 
-char *longest_uniq_subs(char *str) {
-    int size = strlen(str);
 
+int has_uniq_char(char *s) {
+    assert(s);
+    int size = strlen(s);
+    if(size == 1) {
+        return 1;
+    }
+
+    //a b c d
     for(int i = 0; i < size - 1; i++) {
         for(int j = i + 1; j < size; j++) {
-            
+            if(s[i] == s[j]) {
+                return 0;
+            }
         }
+    }
+    return 1;
+}
+
+//a b c d
+char *longest_sub(char *s) {
+    int size = strlen(s);
+    //go throught every substring
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = i + 1; j < size; j++)
     }
 }
 
+
+char *longest_uniq_subs(char *str) {
+    int size = strlen(str);
+    char cur_str[size];
+    int max_len = 0;
+    int cur_len = 0;
+    char max_str[size];
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = i + 1; j < size; j++) {
+            strncpy(cur_str, str + i, j - i);
+            if(Has_unqi_char(cur_str)) {
+                cur_len++;
+            } else {
+                if(cur_len > max_len) {
+                    max_len = cur_len;
+                    strncpy(max_str, cur_str + i, cur_len);
+                    max_str[cur_len] = '\0';
+                    memset(cur_str, 0, size);
+                    cur_len = 0;
+                } else {
+                    cur_len = 0;
+                    memset(cur_str, 0, size);
+                }
+            }        
+        }
+    }
+    printf("%s\n", max_str);
+    return NULL;
+}
+
+
 int main(int argc, int *argv[]) {
-    #if 0
-    test();
+    #if 1
+    char *str = "abcabce";
+    longest_uniq_subs(str);
     #else
     char *str = "abcde";
     char newStr[10] = "xxxxxx";
