84befb9e1c89435ea7de6564697c9c8d32fd2552
MarsWalker <andersluoxt@gmail.com>
2022-12-11 22:23:00 -0800

    test

--- test/test.c
+++ test/test.c
@@ -97,28 +97,111 @@ void longest_common_sub(char *s1, char *s2) {
                 if(tmp1 - p1 > max) {
                     max = tmp1 - p1;
                     strncpy(save, s1 + p1, max);
+                    printf("-> 1: %d, %d, %d\n", p1, tmp1, max);
                 }
+                break;
             } else if(tmp1 == size1) {
                 if(tmp1 - p1 > max) {
                     max = tmp1 - p1;
                     strncpy(save, s1 + p1, max);
+                    printf("-> 2: %d, %d, %d\n", p1, tmp1, max);
                 }
             } else if(tmp2 == size2) {
                 if(tmp2 - p2 > max) {
                     max = tmp2 - p2;
                     strncpy(save, s2 + p2, max);
+                    printf("-> 3: %d, %d, %d\n", p2, tmp2, max);
                 }
             }
         }
     }
     printf("%s\n", save);
 }
+#if 0
+void LCS(char *s1, char *s2) {
+    int i, j, max = 0;
+    int size1 = strlen(s1);
+    int size2 = strlen(s2);
+    char save[100] = {0};
+
+    int dp[100][100] = {0};
+
+    for(i = 0; i < size1; i++) {
+        for(j = 0; j < size2; j++) {
+            if(i == 0 && s1[i] == s2[j]) dp[0][j] = 1;
+            if(j == 0 && s1[i] == s2[j]) dp[i][0] = 1;
+
+            if(s1[i] == s2[j])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+        }
+    }
+
+    for(i = 0; i < size1; i++) {
+        for(j = 0; j < size2; j++) {
+            printf("%d ", dp[i][j]);
+        }
+        printf("\n");
+    }
+
+    int save_index = 0;
+    //find the biggest element
+    for(i = 0; i < size1; i++) {
+        for(j = 0; j < size2; j++) {
+            if(dp[i][j] > max) {
+                max = dp[i][j];
+                save_index = i;
+            }
+        }
+    }
+
+    printf("save_index = %d\n", save_index);
+    while(max > 0) {
+        printf("%c ", s1[save_index - max + 1]);
+        max--;
+    }
+    printf("\n");
+}
+#endif
+
+
+//longest common substring
+void LCS(char *s1, char *s2) {
+    /*
+        dp[i][j]: the length of the common characters in row
+        i, j:     the index element of s1 and s2
+        dp[i][j] = s[i] == s[j] ? dp[i - 1][j - 1] + 1 : 0
+    */
+   int i, j, max = 0;
+   int size1 = strlen(s1);
+    int size2 = strlen(s2);
+    int dp[size1][size2];
+    for(i = 0; i < size1; i++) {
+        for(j = 0; j < size2; j++) {
+            if(s1[0] == s2[j]) {
+                dp[0][j] = 1;
+            }
+            if(s1[i] == s2[0]) {
+                dp[i][0] = 1;
+            }
+            dp[i][j] = s1[i] == s2[j] ? dp[i - 1][j - 1] + 1 : 0;
+        }
+    }
+    //print dp[][]
+    for(i = 0; i < size1; i++) {
+        for(j = 0; j < size2; j++) {
+            printf("%d ", dp[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 
 int main(int argc, char *argv[]) {
-    char s1[] = "abcabcdea";
-    char s2[] = "abcxab";
+    char s1[] = "abxcaxbcdea";
+    char s2[] = "abxcmanxbfc";
 
-    longest_common_sub(s1, s2);
+    //longest_common_sub(s1, s2);
+    LCS(s1, s2);
 
     return 0;
 }
