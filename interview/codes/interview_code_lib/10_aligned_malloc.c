2f633b9765cd30c232417550a992e93ed82f0561
Goodaytodie <andersluo@hotmail.com>
2022-12-29 13:24:25 -0800

    add commit

--- interview/codes/interview_code_lib/10_aligned_malloc.c
+++ interview/codes/interview_code_lib/10_aligned_malloc.c
@@ -17,17 +17,27 @@ typedef uint16_t offset_t;
 
 void *aligned_malloc(size_t required_bytes, size_t alignment)
 {
+    //1. need add a offset to malloc enough space
     int offset = alignment - 1;
     void *p_addr = NULL;
-    //need about 2 bytes for storing offset, to get to orig malloc address during aligned_free()
+
+    /*2. malloc enough space: 
+    1). original memory
+    2). offset
+    3). need extra space to save the difference between address and aligned_address
+    */
     if((p_addr = (void *)malloc(required_bytes + offset + sizeof(offset_t))) == NULL)
     {
         return NULL;
     }
 
+    //3. adjust the aligned_address, to align up the address to meet the alignment
     void *aligned_addr = (void *)(((size_t)p_addr + offset) & ~(offset));
+
+    //4. save the difference
     *((size_t *)aligned_addr - 1) = (size_t)(aligned_addr - p_addr);
 
+    //5. return aligned address
     return aligned_addr;
 }
 
